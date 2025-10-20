c3cf6913119f690773f4d35aec788b6a9725759d
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-10-20 15:44:49 +0700

    update gitignore

--- database/postgreSQL/setup.sql
+++ database/postgreSQL/setup.sql
@@ -1,33 +1,37 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE farmer (
+CREATE SCHEMA auth;
+CREATE SCHEMA farm;
+CREATE SCHEMA analytics;
+
+CREATE TABLE auth.farmer (
     farmer_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
     password_hash TEXT
 );
 
-CREATE TABLE cow (
+CREATE TABLE farm.cow (
     cow_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    farmer_id UUID REFERENCES farmer(farmer_id) ON DELETE CASCADE,
+    farmer_id UUID REFERENCES auth.farmer(farmer_id) ON DELETE CASCADE,
     name VARCHAR(50),
     age INT,
     status INT
 );
 
-CREATE TABLE ai_model (
-    model_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    cow_id UUID REFERENCES cow(cow_id) ON DELETE CASCADE,
-    model BYTEA NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE sensor (
+CREATE TABLE farm.sensor (
     sensor_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     status INT
 );
 
-CREATE TABLE clean_data (
+CREATE TABLE analytics.ai_model (
+    model_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    cow_id UUID REFERENCES farm.cow(cow_id) ON DELETE CASCADE,
+    model BYTEA NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE analytics.clean_data (
     time_generated TIMESTAMP NOT NULL,
     cow_id UUID NOT NULL,
     sensor_id UUID NOT NULL,
@@ -35,6 +39,6 @@ CREATE TABLE clean_data (
     eat_speed INT,
     anomaly_score FLOAT,
     PRIMARY KEY (time_generated, cow_id, sensor_id),
-    FOREIGN KEY (cow_id) REFERENCES cow(cow_id) ON DELETE CASCADE,
-    FOREIGN KEY (sensor_id) REFERENCES sensor(sensor_id)
+    FOREIGN KEY (cow_id) REFERENCES farm.cow(cow_id) ON DELETE CASCADE,
+    FOREIGN KEY (sensor_id) REFERENCES farm.sensor(sensor_id)
 );
