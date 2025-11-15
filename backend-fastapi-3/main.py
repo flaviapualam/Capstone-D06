7a575189bee2cfb8515d342ee8bbe0da88783823
mbudis23 <muhammadbudisetiawan@gmail.com>
2025-11-15 20:36:43 +0700

    try to fix

--- backend-fastapi-3/main.py
+++ backend-fastapi-3/main.py
@@ -50,7 +50,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://192.168.1.10:3000",  # Network access
+        "http://192.168.1.10:3000",
+        "*"  # Network access
     ],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, OPTIONS, etc.)
